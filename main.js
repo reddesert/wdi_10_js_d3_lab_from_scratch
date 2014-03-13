8879e84112de312a68cf404d8ee4a65445113c24
David Fisher <tibbon@gmail.com>
2014-03-13 11:39:44 -0400

    Updated lecture

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-window.onload(function() {
+window.onload = function() {
+  d3.csv("cleaner_data.csv",
+    function(error, rows) {
+      var nested_data = d3.nest()
+                   .key(function(d) { return d.category} )
+                   .entries(rows);
 
-});
+      d3.select("#employment")
+        .selectAll('g')
+        .data(nested_data)
+        .enter()
+          .append('g')
+
+
+    });
+};
