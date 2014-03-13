2b6057f2a677c9fbf335f574b763ead0ebe9825f
David Fisher <tibbon@gmail.com>
2014-03-13 12:54:21 -0400

    Transoformers

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ window.onload = function() {
   d3.csv("cleaner_data.csv",
     function(error, rows) {
       var color = d3.scale.category10();
+      var bar_width = 10;
+      var current_offset = 0;
       var nested_data = d3.nest()
                    .key(function(d) { return d.category} )
                    .entries(rows);
@@ -11,13 +13,17 @@ window.onload = function() {
         .data(nested_data)
         .enter()
           .append('g')
-          .attr('transform', function(d, i) { return "translate(" + i * 40 + ", 0)"})
+          .attr('transform', function(d, i) {
+              if (i > 0) {
+                current_offset += bar_width * nested_data[i - 1].values.length;
+              }
+              return "translate(" + current_offset + ", 0)"})
           .selectAll('rect') // These 3 seem to like each other
-          .data(function(group) { return group.values; })
+          .data(function(category) { return category.values; })
           .enter()
             .append('rect')
-              .attr('width', 12 )
-              .attr('x', function(d, i) { return i * 13 })
+              .attr('width', bar_width - 4 )
+              .attr('x', function(d, i) { return i * (bar_width) })
               .attr('height', function(d) { return d.percent_employed })
               .attr('y', function(d) { return (100 - +d.percent_employed) })
               .style('fill', function(d) { return color(d.category) });
